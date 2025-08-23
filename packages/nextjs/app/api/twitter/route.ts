5dc9c38fa0faedc546665af10c545e99c9112408
NightmareFox12 <carloshenriquez392004@gmail.com>
2025-08-22 20:40:38 -0400

    fix prettier

--- packages/nextjs/app/api/twitter/route.ts
+++ packages/nextjs/app/api/twitter/route.ts
@@ -1,21 +1,21 @@
-import { NextRequest, NextResponse } from "next/server";
+import type { NextRequest } from "next/server";
 
-export async function GET(req: NextRequest) {
+export async function GET(request: NextRequest) {
   try {
-    console.log(req);
+    const url = request.nextUrl;
+    console.log(url);
 
-    return Response.json({ message: "Hello World 2.0" });
+    return Response.json({ message: "Hello World 2.0", url });
   } catch (err) {
     console.log(err);
     return Response.json({ success: false });
   }
 }
 
-export async function POST(res: NextResponse, req: NextRequest) {
+export async function POST(req: Request) {
   try {
     const body = await req.json();
     console.log(req);
-    console.log(res);
 
     return Response.json({ response: body });
   } catch (err) {
