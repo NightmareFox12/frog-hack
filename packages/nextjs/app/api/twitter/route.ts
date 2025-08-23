0a0a79c35dc6bb3847e6fb9a947f48b08a2229f3
NightmareFox12 <carloshenriquez392004@gmail.com>
2025-08-22 20:30:55 -0400

    add post router

--- packages/nextjs/app/api/twitter/route.ts
+++ packages/nextjs/app/api/twitter/route.ts
@@ -1,10 +1,23 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
   try {
     console.log(req);
 
     return Response.json({ message: "Hello World 2.0" });
+  } catch (err) {
+    console.log(err);
+    return Response.json({ success: false });
+  }
+}
+
+export async function POST(req: NextRequest, res: NextResponse) {
+  try {
+    const body = await req.json();
+    console.log(req);
+    console.log(res);
+
+    return Response.json({ response: body });
   } catch (err) {
     console.log(err);
     return Response.json({ success: false });
